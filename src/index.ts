6ddf3c6780c0bddf74d952387755a903620fb722
Alexandr2105 <5030553@gmail.com>
2022-09-14 17:23:21 +0300

    new commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, {Request,Response} from 'express'
+import express, {Request, Response} from 'express';
 
-const app = express()
-const port = process.env.PORT||3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello World!!!';
     res.send(helloMessage);
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
