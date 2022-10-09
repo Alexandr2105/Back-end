59da786a3df5e759ce818df7030cc32451804022
Alexandr2105 <5030553@gmail.com>
2022-10-09 21:26:31 +0300

    new Commit

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -3,12 +3,12 @@ import {usersService} from "../domain/users-service";
 
 export const authRouter = Router();
 
-authRouter.post("/login", async (res: Response, req: Request) => {
-    const checkResult = await usersService.checkUserOrLogin(req.body.loginOrEmail, req.body.password);
+authRouter.post("/login", async (req: Request, res: Response) => {
+    console.log('req.body.login',req.body.login);
+    const checkResult = await usersService.checkUserOrLogin(req.body.login, req.body.password);
     if (checkResult) {
         res.sendStatus(204);
     } else {
         res.sendStatus(401);
     }
 })
-
