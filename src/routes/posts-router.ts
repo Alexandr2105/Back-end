784622ce2a495679ff56afd0611cabdb2be36ee5
Alexandr2105 <5030553@gmail.com>
2022-10-15 18:30:43 +0300

    new Commit

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -71,13 +71,13 @@ postsRouter.get("/:postId/comments", async (req: Request, res: Response) => {
     const query = queryCheckHelper(req.query);
     const comments = await queryRepository.getQueryCommentsByPostId(query, req.params.postId);
     if (comments) {
-        res.status(201).send(comments);
+        res.status(200).send(comments);
     } else {
         res.sendStatus(404);
     }
 });
 
-postsRouter.post("/:postId/comments", checkToken, contentLengthByPostId, async (req: Request, res: Response) => {
+postsRouter.post("/:postId/comments", checkToken, contentLengthByPostId, middleWare, async (req: Request, res: Response) => {
     const post = await postsService.creatNewCommentByPostId(req.params.postId, req.body.content, req.user!.id, req.user!.login);
     const newPost = await commentService.getCommentById(post.id);
     res.send(newPost);
