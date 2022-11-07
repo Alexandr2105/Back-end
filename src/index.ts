5367f58a2e4569eae1fab41686113aeab8e9c081
Alexandr2105 <5030553@gmail.com>
2022-11-07 22:03:13 +0300

    new Commit

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {runDb} from "./db/db";
 import {usersRouter} from "./routes/users-router";
 import {authRouter} from "./routes/auth-router";
 import {commentsRouter} from "./routes/comments-router";
+import {securityDevicesRouter} from "./routes/securityDevices-router";
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/comments", commentsRouter);
+app.use("/security/devices", securityDevicesRouter);
 
 app.get("/", (req: Request, res: Response) => {
     let helloMessage = 'Hello World!!!';
---
+++ src/routes/securityDevices-router.ts
@@ -0,0 +1,17 @@
+import {Router, Request, Response} from "express";
+import {checkToken} from "../middlewares/middleware";
+
+export const securityDevicesRouter = Router();
+
+securityDevicesRouter.get("/", checkToken, (req: Request, res: Response) => {
+    console.log(req.ip);
+    res.sendStatus(200);
+});
+
+securityDevicesRouter.delete("/", checkToken, (req: Request, res: Response) => {
+    res.sendStatus(204);
+});
+
+securityDevicesRouter.delete("/:devices", checkToken, (req: Request, res: Response) => {
+    res.sendStatus(204);
+});
