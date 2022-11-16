50ed0aad49642c2c21e345fdef84c6a11151430f
Alexandr2105 <5030553@gmail.com>
2022-11-16 21:49:41 +0300

    new Commit

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -13,12 +13,13 @@ export const emailAdapter = {
         const info = await transporter.sendMail({
             from: 'Alex <testnodemaileremail@gmail.com>',
             to: email,
-            subject: "Password recovery",
-            html:`<h1>Password recovery</h1>
-                       <p>To finish password recovery please follow the link below:
-                          <a href='https://somesite.com/confirm-email?code=${confirm}'>recovery password</a>
+            subject: "Registration",
+            html:`<h1>Thank for your registration</h1>
+                       <p>To finish registration please follow the link below:
+                          <a href='https://somesite.com/confirm-email?code=${confirm}'>complete registration</a>
                         </p>`
         });
+        console.log(confirm);
         return true;
     }
 }
