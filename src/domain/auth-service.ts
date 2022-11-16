fb38b8e7b88235b167268bda09d1043f8d91fb4c
Alexandr2105 <5030553@gmail.com>
2022-11-16 22:01:12 +0300

    new Commit

--- src/domain/auth-service.ts
+++ src/domain/auth-service.ts
@@ -28,14 +28,11 @@ export const authService = {
         if (user.expirationDate < new Date()) return false;
         return await usersRepository.updateEmailConfirmation(user.userId);
     },
-    // async getNewConfirmationCode(email: string) {
-    //     const newCode = uuid4();
-    //     const updateCode = await usersRepository.setConfirm(email, newCode);
-    //     if (updateCode) {
-    //         return newCode;
-    //     }
-    // }
     async getNewConfirmationCode(email: string) {
-        return uuid4();
+        const newCode = uuid4();
+        const updateCode = await usersRepository.setConfirm(email, newCode);
+        if (updateCode) {
+            return newCode;
+        }
     }
 };
