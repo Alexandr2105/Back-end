6241b8d13c9bb2b1e7d5518d3c71bd3c47f1626a
Alexandr2105 <5030553@gmail.com>
2022-11-17 12:46:01 +0300

    new Commit

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -14,11 +14,26 @@ export const emailAdapter = {
             from: 'Alex <testnodemaileremail@gmail.com>',
             to: email,
             subject: "Registration",
-            html:`<h1>Thank for your registration</h1>
-                       <p>To finish registration please follow the link below:
-                          <a href='https://somesite.com/confirm-email?code=${confirm}'>complete registration</a>
-                        </p>`
+            html: `<h1>Password recovery</h1>
+                <p>To finish password recovery please follow the link below:
+                <a href='https://somesite.com/password-recovery?recoveryCode=${confirm}'>recovery password</a>
+                </p>`
+            // html:`<h1>Thank for your registration</h1>
+            //            <p>To finish registration please follow the link below:
+            //               <a href='https://somesite.com/confirm-email?code=${confirm}'>complete registration</a>
+            //             </p>`
         });
         return true;
     }
 }
+
+//     <h1>Password recovery</h1>
+// <p>To finish password recovery please follow the link below:
+//     <a href='https://somesite.com/password-recovery?recoveryCode=your_recovery_code'>recovery password</a>
+// </p>
+//
+//
+// <h1>Thank for your registration</h1>
+// <p>To finish registration please follow the link below:
+//     <a href='https://somesite.com/confirm-email?code=your_confirmation_code'>complete registration</a>
+// </p>
