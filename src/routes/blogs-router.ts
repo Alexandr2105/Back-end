39a8ec58ea9bc213b95cbd7982973e1227d85125
Alexandr2105 <5030553@gmail.com>
2022-11-20 13:19:09 +0300

    new Commit

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -10,7 +10,7 @@ import {queryCheckHelper} from "../helper/queryCount";
 export const blogsRouter = Router();
 
 const nameLength = body("name").isLength({max: 15}).withMessage("Длина больше 15 символов").trim().notEmpty().withMessage("Это поле должно быть заплнено");
-const urlLength = body("youtubeUrl").isLength({max: 100}).trim().notEmpty().isURL().withMessage("Не верно заполнено поле");
+const urlLength = body("websiteUrl").isLength({max: 100}).trim().notEmpty().isURL().withMessage("Не верно заполнено поле");
 const titleLength = body("title").trim().notEmpty().isLength({max: 30}).withMessage("Не верно заполнено поле");
 const shortDescriptionLength = body("shortDescription").trim().notEmpty().isLength({max: 100}).withMessage("Не верно заполнено поле");
 const contentLength = body("content").trim().notEmpty().isLength({max: 1000}).withMessage("Не верно заполнено поле");
@@ -49,13 +49,13 @@ blogsRouter.delete("/:id", aut, async (req: Request, res: Response) => {
 });
 
 blogsRouter.post("/", aut, nameLength, urlLength, description, middleWare, async (req: Request, res: Response) => {
-    const createBlog = await blogsService.createBlog(req.body.name, req.body.youtubeUrl, req.body.description);
+    const createBlog = await blogsService.createBlog(req.body.name, req.body.websiteUrl, req.body.description);
     const newBlog = await blogsService.getBlogsId(createBlog.id);
     res.status(201).send(newBlog);
 });
 
 blogsRouter.put("/:id", aut, nameLength, urlLength, middleWare, async (req: Request, res: Response) => {
-    const updateBlog = await blogsService.updateBlog(req.params.id, req.body.name, req.body.youtubeUrl);
+    const updateBlog = await blogsService.updateBlog(req.params.id, req.body.name, req.body.websiteUrl);
     if (updateBlog) {
         res.sendStatus(204);
     } else {
