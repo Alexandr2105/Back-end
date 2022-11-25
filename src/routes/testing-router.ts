db5ae26704dde9f4c03a0b71f95fc8d9e1e450e1
Alexandr2105 <5030553@gmail.com>
2022-11-25 15:25:34 +0300

    new Commit

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -3,7 +3,7 @@ import {testingRepository} from "../repositories/testing-repository";
 
 export const testingRouter = Router();
 
-testingRouter.delete("/all-data", (req: Request, res: Response) => {
-    testingRepository.deleteAllCollection();
+testingRouter.delete("/all-data", async (req: Request, res: Response) => {
+    await testingRepository.deleteAllCollection();
     res.sendStatus(204);
 });
